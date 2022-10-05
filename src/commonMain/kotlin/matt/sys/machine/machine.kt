0aeff6fbbbc4226513037b7142daa1c0f42c6b0c
mgroth0 <mgroth49@gmail.com>
2022-10-05 00:18:46 -0400

    first commit

---
+++ src/commonMain/kotlin/matt/sys/machine/machine.kt
@@ -0,0 +1,37 @@
+package matt.sys.machine
+
+import matt.lang.shutdown.duringShutdown
+import matt.model.tostringbuilder.toStringBuilder
+
+abstract class Machine {
+
+  private var didFirstBoot = false
+  private var on: Boolean = false
+
+  override fun toString() = toStringBuilder(::on)
+
+  @Synchronized fun start() {
+	if (!didFirstBoot) {
+	  duringShutdown {
+		shutdown()
+	  }
+	}
+	if (!on) boot()
+	didFirstBoot = true
+	on = true
+  }
+
+  @Synchronized fun shutdown() {
+	println("shutting down $this")
+	if (on) unBoot()
+	on = false
+  }
+
+  @Synchronized fun restart() {
+	shutdown()
+	start()
+  }
+
+  protected abstract fun boot()
+  protected abstract fun unBoot()
+}
