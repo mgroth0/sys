dd1c58bb47f3f53015ec7eadcff79420a38796c7
mgroth0 <mgroth49@gmail.com>
2022-10-10 00:20:25 -0400

    autocommit

--- src/jvmMain/kotlin/matt/sys/loopthread/loopthread.kt
+++ src/jvmMain/kotlin/matt/sys/loopthread/loopthread.kt
@@ -1,18 +1,23 @@
 package matt.sys.loopthread
 
-import matt.log.profile.StructuredExceptionHandler
+import matt.log.profile.err.StructuredExceptionHandler
 import matt.model.flowlogic.controlflowstatement.ControlFlow
 import matt.model.flowlogic.controlflowstatement.ControlFlow.BREAK
 import matt.model.flowlogic.controlflowstatement.ControlFlow.CONTINUE
-import matt.model.idea.ProceedingIdea
+import matt.model.startstop.Stoppable
 import kotlin.time.Duration
 
 class MutableRefreshTimeDaemonLoop(
-  sleep: Duration,
+  sleepInterval: Duration,
   op: DaemonLoop.()->ControlFlow,
   finalize: ()->Unit = {},
   uncaughtExceptionHandler: StructuredExceptionHandler? = null
-): DaemonLoop(sleep, op = op, finalize = finalize, uncaughtExceptionHandler = uncaughtExceptionHandler) {
+): DaemonLoop(
+  sleepInterval,
+  op = op,
+  finalize = finalize,
+  uncaughtExceptionHandler = uncaughtExceptionHandler
+) {
   public override var sleepInterval = super.sleepInterval
 }
 
@@ -21,15 +26,18 @@ open class DaemonLoop(
   private val op: DaemonLoop.()->ControlFlow,
   private val finalize: ()->Unit = {},
   uncaughtExceptionHandler: StructuredExceptionHandler? = null
-): Thread(), ProceedingIdea {
+): Thread(), Stoppable {
 
 
   private var shouldContinue: Boolean = true
 
-  val wasSignaledToStop get() = !shouldContinue
-
-  fun signalToStop() {
+  override fun sendStopSignal() {
 	shouldContinue = false
+  }
+
+  override fun stopAndJoin() {
+	sendStopSignal()
+	join()
   }
 
   init {
@@ -50,8 +58,8 @@ open class DaemonLoop(
 
 		BREAK    -> break
 	  }
-	  finalize()
 	}
+	finalize()
   }
 }
 
