08a2fd58a1bde354f22fb487b94edda3f39669d8
mgroth0 <mgroth49@gmail.com>
2023-09-19 00:38:34 -0400

    changed src/commonMain/kotlin/matt/sys/machine/machine.kt

--- src/commonMain/kotlin/matt/sys/machine/machine.kt
+++ src/commonMain/kotlin/matt/sys/machine/machine.kt
@@ -1,41 +1,44 @@
 package matt.sys.machine
 
 import matt.lang.idea.ProceedingIdea
-import matt.lang.shutdown.duringShutdown
+import matt.lang.shutdown.ShutdownContext
 import matt.reflect.tostring.toStringBuilder
 
 
+context(ShutdownContext)
+abstract class Machine() : ProceedingIdea {
 
-abstract class Machine: ProceedingIdea {
+    private var didFirstBoot = false
+    private var on: Boolean = false
 
-  private var didFirstBoot = false
-  private var on: Boolean = false
-
-  override fun toString() = toStringBuilder(::on)
+    override fun toString() = toStringBuilder(::on)
 
 
-  @Synchronized fun start() {
-	if (!didFirstBoot) {
-	  duringShutdown {
-		shutdown()
-	  }
-	}
-	if (!on) boot()
-	didFirstBoot = true
-	on = true
-  }
+    @Synchronized
+    fun start() {
+        if (!didFirstBoot) {
+            duringShutdown {
+                shutdown()
+            }
+        }
+        if (!on) boot()
+        didFirstBoot = true
+        on = true
+    }
 
-  @Synchronized fun shutdown() {
-	println("shutting down $this")
-	if (on) unBoot()
-	on = false
-  }
+    @Synchronized
+    fun shutdown() {
+        println("shutting down $this")
+        if (on) unBoot()
+        on = false
+    }
 
-  @Synchronized fun restart() {
-	shutdown()
-	start()
-  }
+    @Synchronized
+    fun restart() {
+        shutdown()
+        start()
+    }
 
-  protected abstract fun boot()
-  protected abstract fun unBoot()
+    protected abstract fun boot()
+    protected abstract fun unBoot()
 }
